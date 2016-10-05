4ebffe49ecadd6411c2218c7e5d1f62542b9b3c9
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2016-10-05 14:14:51 -0400

    Added weather and time

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io').listen(http);
+var Forecast = require('forecast');
+var CronJob = require('cron').CronJob;
 
 var port = 80;
 
@@ -18,22 +20,55 @@ app.get('/', function(req, res) {
 http.listen(port);
 console.log('Server listening at port ' + port);
 
+var forecast = new Forecast({
+    service: 'darksky', 
+    key: '1cd9840453629845d4c54ec81305eee0',
+    units: 'celsius', 
+    cache: true,
+    ttl: {
+        minutes: 27,
+        seconds: 45
+    }
+})
+
 board.on("ready", function() {
 	strip = new pixel.Strip({
 		board: this,
 		controller: "FIRMATA",
 		strips: [ {pin: 6, length: 150}]
 	});
+
+    strip.on("ready", function() {
+        // Display time every second
+        new CronJob('* * * * * *', function() {
+            var d = new Date();
+            showTime(d, 16);
+            strip.show();
+        }, null, true, 'America/Los_Angeles');
+
+        forecast.get([42.2626, -71.8023], function(err, weather) {
+            if (err) console.log(err);
+            showWeather(weather, 0);
+        })
+
+        // Display weather every hour
+        new CronJob('0 * * * * *', function() {
+            forecast.get([42.2626, -71.8023], function(err, weather) {
+                if (err) console.log(err);
+                showWeather(weather, 0);
+            })
+        })
+    })
 })
-
-
 
 io.on('connection', function(socket) {
 	console.log(socket.id);
 
 	socket.on('setColor', function(colorString) {
 		console.log("Setting color " + colorString);
-		strip.color(colorString);
+        for (var i=36;i<150;i++) {
+            strip.pixel(i).color(colorString);
+        }
 		strip.show();
 	})
 
@@ -46,10 +81,101 @@ io.on('connection', function(socket) {
 	})
 })
 
- function dynamicRainbow( delay ){
-        console.log( 'dynamicRainbow' );
 
-        var showColor;
+// Parse time object and display it on the strip at the startPixel 
+function showTime(date, startPixel) { //Best function name 2016
+    var hours = date.getHours().toString(2);
+    var minutes = date.getMinutes().toString(2);
+    var seconds = date.getSeconds().toString(2);
+
+    seconds = "000000".substr(seconds.length)+seconds;
+    minutes = "000000".substr(minutes.length)+minutes;
+    hours   = "00000" .substr(hours  .length)+hours  ;
+
+    timeStr =  "-" + hours + "-" + minutes + "-" + seconds;
+
+    for (var i = 0; i < timeStr.length; i++) {
+        switch(timeStr[i]) {
+            case '0':
+                strip.pixel(i+startPixel).color('black');
+            break;
+            case '1':
+                strip.pixel(i+startPixel).color('white');
+            break;
+            case '-':
+                strip.pixel(i+startPixel).color('blue');
+            break;
+        }
+    }
+}
+
+function showWeather(weather, startPixel) {
+    var colorScale = [  "silver", "silver",     //-20
+                        "gray", "gray",         //-10
+                        "white", "aqua",        //0
+                        "teal", "blue",         //10
+                        "blue", "navy",         //20
+                        "aquamarine", "orange", //30
+                        "yellow", "red"         //40
+                        ];
+    var hourlyData = weather.hourly.data[0];
+    var hourlyTemp = hourlyData.apparentTemperature;
+    var hourlyIcon = weather.hourly.icon;
+
+     switch (hourlyIcon) {
+        case "clear-day":
+            var weatherColors = ["lightskyblue", "lightskyblue"]
+        break;
+        case "clear-night":
+            var weatherColors = ["lightskyblue", "midnightblue"];
+        break;
+        case "rain":
+            var weatherColors = ["navy", "navy"];
+        break;
+        case "snow":
+            var weatherColors = ["azure", "azure"];
+        break;
+        case "sleet":
+            var weatherColors = ["azure", "lightslategray"];
+        break;
+        case "wind":
+            var weatherColors = ["mediumspringgreen", "mediumspringgreen"]
+        break;
+        case "fog":
+            var weatherColors = ["gray", "gray"]
+        break;
+        case "cloudy":
+            var weatherColors = ["silver", "silver"];
+        break;
+        case "partly-cloudy-day":
+            var weatherColors = ["silver", "lightskyblue"];
+        break;
+        case "partly-cloudy-night":
+            var weatherColors = ["silver", "midnightblue"];
+        break;
+    }
+
+    weatherColors.push("black");
+
+    var tempScale = parseInt((hourlyTemp+20)/5);
+    var tempStr = new String();
+    for (var i=0;i<12;i++){
+        if(i<tempScale) {
+            weatherColors.push(colorScale[i])
+        } else weatherColors.push("black");
+    } // Convert to scale
+
+    console.log(weatherColors);
+
+    for(var i=0;i<weatherColors.length;i++) {
+        strip.pixel(i + startPixel).color(weatherColors[i])
+    }
+}
+
+function dynamicRainbow( delay ){
+    console.log( 'dynamicRainbow' );
+
+    var showColor;
         var cwi = 0; // colour wheel index (current position on colour wheel)
         rainbowInterval = setInterval(function(){
             if (++cwi > 255) {
@@ -89,4 +215,4 @@ io.on('connection', function(socket) {
         return "rgb(" + r +"," + g + "," + b + ")";
     }
 
-console.log("Waiting for connection..");
+    console.log("Waiting for connection..");
--- public/index.js
+++ public/index.js
@@ -1,20 +1,43 @@
 window.onload = function() {
 
-	var socket = io();
-	var cw = Raphael.colorwheel($(".colorwheel")[0], 300, 180);
-	cw.onchange(function(color) {
-		console.log(color.hex);
-		socket.emit('setColor', color.hex);
+	// var socket = io();
+	// var cw = Raphael.colorwheel($(".colorwheel")[0], 300, 180);
+	// cw.onchange(function(color) {
+	// 	console.log(color.hex);
+	// 	socket.emit('setColor', color.hex);
+	// })
+
+	colors = ['#ffffff', "#000fff", "#fff000"];
+
+	svgContainer = d3.select("body").append("svg")
+	.attr("width", 200)
+	.attr("height", 200);
+
+	update(svgContainer);
+
+}
+
+function update(svg) {
+	var rects = svg
+	.selectAll("rect")
+	.data(colors)
+	.enter()
+	.append("rect");
+
+	var rectsattributes = rects
+	.attr("x", function(d, i) {
+		return i*10;
 	})
-
-	$('#rainbow').toggle(
-		function() {
-			socket.emit('rainbowOn');
-	},
-		function() {
-			socket.emit('rainbowOff');
-			socket.emit('setColor', 'red');
-		}
-	)
-
+	.attr("y", function(d, i) {
+		return 0;
+	})
+	.attr("width", function(d) {
+		return 10;
+	})
+	.attr("height", function(d) {
+		return 10;
+	})
+	.style("fill", function (d) {
+		return d;
+	})
 }

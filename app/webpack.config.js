b9d7b875cb75ab1aa3bba2e4db65360996865785
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2017-08-20 18:56:50 -0400

    Used ivanempire's boilerplate cause me dumb two

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,35 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+const path = require("path");
+const webpack = require("webpack");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+//   template: './index.html',
+//   filename: 'index.html',
+//   inject: 'body'
+// });
+
+const HotModuleReplacementPluginConfig = new webpack.HotModuleReplacementPlugin();
 
 module.exports = {
-	entry: './src/index.jsx',
+	entry:[
+		"react-hot-loader/patch",
+    	"webpack-hot-middleware/client?http://localhost:3000&reload=true",
+    	"./public/index.jsx"	
+	],
 	output: {
-		filename: "index.js",
-		path: path.resolve(__dirname, 'build/'),
-    publicPath: "/build"
+		path:path.join(__dirname,"public"),
+		filename:"bundle.js",
+		publicPath:"/dist/"
 	},
-  	resolve: {
-    	extensions: ['*', '.js', '.jsx', '.json', '.css']
-  	},
-  	module: {
-    	loaders: [{
-      	test: /\.jsx?$/,
-      	exclude: /node_modules/,
-      	loader: ['babel-loader']
-    	},
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }]
-  },
-  plugins: [HtmlWebpackPluginConfig],
-  devServer: {
-    contentBase: "./build",
-    hot: true
-  }
-};
+	resolve: {
+		extensions: ['*', '.js', '.jsx', '.json', '.css']
+	},
+	module: {
+		loaders:[
+		{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			loader: "babel-loader",
+			query: {
+				presets:["es2015","react"]
+			}
+		},
+		{
+			test: /\.html$/,
+			loader:"html-loader"
+		},
+		{
+			test: /\.css$/,
+			loader: ["style-loader", "css-loader"]
+		}]
+	},
+	plugins: [
+		// HtmlWebpackPluginConfig, 
+		HotModuleReplacementPluginConfig],
+	devServer: {
+		contentBase: "/dist/",
+		hot: true
+	}
+}
